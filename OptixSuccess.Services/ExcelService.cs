3559725ea05513986666c0fcbca1da064290cbed
kudesnik <kudesnik.Net@gmail.com>
2022-02-06 00:50:17 +0300

    Solution transfer to another machine

---
+++ OptixSuccess.Services/ExcelService.cs
@@ -0,0 +1,51 @@
+﻿using OfficeOpenXml;
+
+namespace OptixSuccess.Services
+{
+    public static class ExcelService
+    {
+        public static byte[] GenerateExcelWorkbook()
+        {
+            var list = new List<UserInfo>()
+        {
+            new UserInfo { UserName = "catcher", Age = 18 },
+            new UserInfo { UserName = "james", Age = 20 },
+        };
+            var stream = new MemoryStream();
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage(stream))
+            {
+                var workSheet = package.Workbook.Worksheets.Add("Sheet1");
+
+                // simple way
+                workSheet.Cells.LoadFromCollection(list, true);
+
+                ////// mutual
+                ////workSheet.Row(1).Height = 20;
+                ////workSheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                ////workSheet.Row(1).Style.Font.Bold = true;
+                ////workSheet.Cells[1, 1].Value = "No";
+                ////workSheet.Cells[1, 2].Value = "Name";
+                ////workSheet.Cells[1, 3].Value = "Age";
+
+                ////int recordIndex = 2;
+                ////foreach (var item in list)
+                ////{
+                ////    workSheet.Cells[recordIndex, 1].Value = (recordIndex - 1).ToString();
+                ////    workSheet.Cells[recordIndex, 2].Value = item.UserName;
+                ////    workSheet.Cells[recordIndex, 3].Value = item.Age;
+                ////    recordIndex++;
+                ////}
+
+                return package.GetAsByteArray();
+            }
+        }
+    }
+
+    public class UserInfo
+    {
+        public string? UserName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Plugins.OsExcel/Worker.cs
@@ -0,0 +1,45 @@
+﻿using ClosedXML.Excel;
+using CoreBusiness;
+
+namespace Plugins.OsExcel
+{
+    public class Worker
+    {
+        public XLWorkbook GetProductsXLWorkbook()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product { ProductId = 1, CategoryId = 1, Name = "SofLens 59 6 pk", BC = " 8.6", Sph = "-0.50", Cell = "C4", Quantity = 1, Price = 1.00},
+                new Product { ProductId = 2, CategoryId = 1, Name = "SofLens 59 6 pk", BC = " 8.6", Sph = "-0.75", Cell = "C5", Quantity = 1, Price = 1.00},
+                new Product { ProductId = 3, CategoryId = 1, Name = "SofLens 59 6 pk", BC = " 8.6", Sph = "-1.00", Cell = "C6", Quantity = 1, Price = 1.00},
+                new Product { ProductId = 4, CategoryId = 1, Name = "SofLens 59 6 pk", BC = " 8.6", Sph = "-1.25", Cell = "C7", Quantity = 1, Price = 1.00},
+                new Product { ProductId = 5, CategoryId = 1, Name = "SofLens 59 6 pk", BC = " 8.6", Sph = "-1.50", Cell = "C8", Quantity = 1, Price = 1.00}
+            };
+
+
+            var workbook = new XLWorkbook();
+            IXLWorksheet worksheet =
+            workbook.Worksheets.Add("Products");
+            worksheet.Cell(1, 1).Value = "Name";
+            worksheet.Cell(1, 2).Value = "BC";
+            worksheet.Cell(1, 2).Value = "Sph";
+
+            for (int i = 1; i <= 2; i++)
+            {
+                worksheet.Cell(1, i).Style.Font.Bold = true;
+            }
+
+            int index = 1;
+            foreach (var product in products)
+            {
+                worksheet.Cell(index + 1, 1).Value = product.Name;
+                worksheet.Cell(index + 1, 2).Value = product.BC;
+                worksheet.Cell(index + 1, 2).Value = product.Sph;
+
+                index++;
+            }
+
+            return workbook;          
+        }
+    }
+}
